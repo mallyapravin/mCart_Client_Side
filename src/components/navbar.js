dc4e0a81edf617df5fad9fcc95fd0fe68d409dd2
mallyapravin@gmail.com <mallyapravin@gmail.com>
2021-05-28 10:13:29 +0530

    navbar styles added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,21 +16,26 @@ class Navbar extends React.Component{
 
             <>
 <Router>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#"><Link to="/">GadgetMart</Link></a>
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark primary-color">
+  <a className="navbar-brand" href="/">GadgetMart</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="/products">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Categories</a>
+        <a className="nav-link" href="/products">Home</a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/products/search/camera">Smartphones</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
     </ul>
   </div>
